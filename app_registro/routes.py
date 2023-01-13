9b3e71a758f8bb7b6148a4e60d88f5c716bb9de4
pettdev <pettdev@gmail.com>
2023-01-13 10:09:53 -0400

    funcion eliminar en progreso

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -1,7 +1,7 @@
 from app_registro import app
 from flask import render_template,request,redirect
 import csv
-from datetime import datetime,date
+from datetime import date
 
 @app.route("/")
 def index():
@@ -14,34 +14,42 @@ def index():
     #recorrer el objeto csvReader y cargar cada registro al array datos
     for item in csvReader:
         datos.append(item)
+    fichero.close()    
     return render_template("index.html",pageTitle="Listas",lista=datos)
 
 
 @app.route("/new",methods=["GET","POST"])
 def create():
     if request.method == "GET":#esto puede ser POST o GET
-        return render_template("new.html",pageTitle="Alta",typeAction="Alta",typeButon="Guardar")   
+        return render_template("new.html",pageTitle="Alta",typeAction="Alta",typeButon="Guardar",dataForm={})   
     else:
-        #acceder al archivo y configurarlo para cargar un nuevo registro
-        mifichero =  open('data/movimientos.csv','a',newline='')
-        #llamamos al metodo writer de escritura y cargamos el formeto para csv
-        lectura= csv.writer(mifichero, delimiter=',',quotechar='"')
-        
-        #realizar el control de fecha
-        date_object = datetime.strptime(request.form['date'], '%Y-%M-%d').date()
 
-        #-dar fomato final de fechas
-        #-validar que el request.form.date <= la fecha de hoy
-        #-si date > hoy devolver un formulario vacio get de new.html
+        error = validateForm(request.form)#validamos los datos de formulario
 
+        if error:
+            #hay error
+            return render_template("new.html",pageTitle="Alta",typeAction="Alta",typeButon="Guardar",msgError=error,dataForm=request.form)
+        else: 
 
-        if date_object < date.today():
-            print("fecha correcta")
-        else:
-            print("fecha incorrecta")
-        
-        #registramos los datos recibidos desde el formulario con request.form y lo añadimos con el metodo writerrow
-        lectura.writerow([request.form['date'],request.form['concept'],request.form['quantity']])    
+           
+            mifichero =  open('data/movimientos.csv','a',newline='')
+            lectura= csv.writer(mifichero, delimiter=',',quotechar='"')
+            
+            #crear id
+            fichero = open("data/last_id.csv","r")
+            registro = fichero.read()
+            if registro == "":
+                new_id=1
+            else:    
+                new_id = int(registro)+1
+            
+            fichero.close()
+
+            ficheroG =  open('data/last_id.csv','w')
+            ficheroG.write(str(new_id))
+            ficheroG.close()
+
+            lectura.writerow([new_id,request.form['date'],request.form['concept'],request.form['quantity']])    
 
         mifichero.close()
 
@@ -50,10 +58,67 @@ def create():
 
         
    
-@app.route("/update")
-def edit():
-    return render_template("update.html",pageTitle="Modificación",typeAction="Modificación",typeButon="Editar") 
+@app.route("/update/<int:id>")
+def edit(id):
+    return render_template("update.html",pageTitle="Modificación",typeAction="Modificación",typeButon="Editar",dataForm={}) 
+    #return f"este es el id={id} del registro a modificar"
 
-@app.route("/delete")
-def remove():
-    return render_template("delete.html",pageTitle="Eliminar")
+@app.route("/delete/<int:id>", methods=["GET","POST"])
+def remove(id):
+
+    #1-consultar en data/movimientos.csv y recuperar el registro con id de la peticion
+    #2-devolver al formulario html para borrar que los campos no sean modificables
+    #3-tendria un boton para confirmar el borrado, si da accion a este boton borrar el registro dado
+    if request.method == "GET":
+
+        mifichero =  open('data/movimientos.csv','r')
+        lectura= csv.reader(mifichero, delimiter=',',quotechar='"')
+        registro_buscado=[]#len 0
+        for registro in lectura:
+            if registro[0] == str(id):
+                #aqui encuentra el dato
+                registro_buscado = registro
+        mifichero.close()
+        #que hacemos si no encuentra registro
+
+        if len(registro_buscado) > 0:
+            return render_template("delete.html",pageTitle="Eliminar",registros=registro_buscado)
+        else:
+           return redirect("/")
+    else:#aqui seria post
+        return f"Debemos de borrar el archivo con este id={id}"
+        '''
+        fichero_old =  open('data/movimientos.csv','r')
+        fichero = open('data/movimientos_new.csv','w',newline="")
+
+        csvReader= csv.reader(fichero_old, delimiter=',',quotechar='"')
+        csvWriter = csv.writer( fichero , delimiter=',',quotechar='"')
+
+        for registro in csvReader:
+            if registro[0] != str(id):#mientras el id sea distinto del proporcionado para borrar que escriba en fichero
+                csvWriter.writerow(registro)
+
+        fichero_old.close()
+        fichero.close()    
+
+        return redirect("/")     
+        '''
+
+
+
+
+#crear una funcion para validar formulario de registro donde controlemos lo siguiente:
+#1-que la fecha no sea mayor a la actual
+#2-que el concepto no vaya vacio
+#3-que la cantidad sea distinto de 0 y de vacio
+
+def validateForm(requestForm):
+    hoy = date.today().isoformat()
+    errores=[]
+    if requestForm['date'] > hoy:
+        errores.append("fecha invalida: La fecha introducida es a futuro")
+    if requestForm['concept'] == "":
+        errores.append("concepto vacio: Introduce un concepto para el registro")
+    if requestForm['quantity'] == "" or float(requestForm['quantity']) == 0.0:
+        errores.append("cantidad vacio o cero: Introduce una cantidad positiva o negativa")   
+    return errores
--- ejemplos.py
+++ ejemplos.py
@@ -27,7 +27,7 @@ lectura.writerow(['19/12/2022','Compra de arbol de navidad',-100.50])
 
 mifichero.close()
 '''
-
+'''
 from datetime import datetime,date
 
 date_str = '09-19-2022'
@@ -38,3 +38,17 @@ print(date_object)  # printed in default format
 
 print("fechalocal",date.today())
 print("fechalocal",type(date.today()))
+'''
+
+
+        
+fichero = open("data/movimientos.csv","r")
+csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
+lista_id=[]
+for item in csvReader:
+    lista_id.append(item[0])
+
+#print(lista_id)
+id = lista_id[ len(lista_id)-1 ]#obtendria el ultimo id
+print(type(id))
+     
